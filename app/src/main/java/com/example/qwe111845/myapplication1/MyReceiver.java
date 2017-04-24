ac9ee4bb7807fff3c2ded9df4e0024167df48e23
qwe111845 <qwe111845@gmail.com>
2017-04-25 03:48:44 +0800

    第一

---
+++ app/src/main/java/com/example/qwe111845/myapplication1/MyReceiver.java
@@ -0,0 +1,43 @@
+package com.example.qwe111845.myapplication1;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiver extends BroadcastReceiver {
+    static int id = 5000;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String msg = intent.getStringExtra("KEY_MSG");
+        Intent newintent = new Intent();
+        newintent.setClass(context, MainActivity1.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, newintent, 0);
+        Notification notify = newNotification(context, pendingIntent,
+                "(New) Broadcast is received.",msg);
+        NotificationManager notificationManager =(NotificationManager)context.
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(id++, notify);
+    }
+    @SuppressLint("NewApi")
+    private Notification newNotification(
+            Context context, PendingIntent pi,
+            String title, String msg) {
+        Notification.Builder builder =
+                new Notification.Builder(context);
+
+        builder.setContentTitle(title);
+        builder.setContentText(msg);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(pi);
+        builder.setTicker(msg);
+        builder.setWhen(System.currentTimeMillis());
+        Notification notify = builder.build();
+        return notify;
+    }
+
+}
